abd04d090aba23ca7e2dc73627e41c95854d5ece
GustavoMacias <gustavo.maci.1234@gmail.com>
2025-11-24 17:26:37 -0600

    Fix: Replace next.config.ts with next.config.js for GitHub Pages

--- app/hola/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProyectosPHP() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold">Proyectos en PHP</h1>
-        </div>
-    );
-}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production'
 const repoName = 'gustavo-portafolio'
