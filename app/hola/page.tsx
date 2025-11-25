b1fbf2517fda1fc1e8e3fcd9308873f8d60ea8c1
GustavoMacias <gustavo.maci.1234@gmail.com>
2025-11-24 18:03:07 -0600

    prueba 2

--- app/hola/page.tsx
+++ app/hola/page.tsx
@@ -2,6 +2,11 @@ export default function Hola() {
     return (
         <main>
             <h1>Hola</h1>
+            <div>
+                <p>hola que tal</p>
+            </div>
+
+            <h1>Adios</h1>
         </main>
     )
 }
